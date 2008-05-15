f55560404785a63bb0c8604034adbec715bf539b
Coda Hale <coda.hale@gmail.com>
2008-05-14 22:00:19 -0700

    Added spec helper for fixture executables.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require "rubygems"
 require "spec"
+
+Spec::Runner.configure do |config|
+  def fixture(name)
+    File.join(".", "spec", "fixtures", "#{name}.rb")
+  end
+end
