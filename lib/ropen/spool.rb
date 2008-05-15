949057c781d8444cc4d87c5dcf915b1a065d6e89
Coda Hale <coda.hale@gmail.com>
2008-05-15 16:33:54 -0700

    Document Ropen::Spool.

--- lib/ropen/spool.rb
+++ lib/ropen/spool.rb
@@ -1,20 +1,35 @@
 require "ropen"
 
-# TODO: document me
-
+# A recorder/replayer for messages sent to a receiver. Allows clients to spool a
+# series of messages which can then be sent in order to a specified receiver.
+# 
+# ==== Examples
+#   
+#   receiver = "You "
+#   spool = Ropen::Spool.new
+#   spool << " and me."
+#   spool.upcase!
+#   spool.replay(receiver)
+#   receiver #=> "YOU AND ME."
+#   
 class Ropen::Spool
+  
+  # Creates a new, empty spool.
   def initialize
     @messages = []
   end
   
+  # Registers the method call as a message to send to the eventual receiver.
   def method_missing(m, *args, &block)
     @messages << [m, args, block]
   end
   
-  def replay(stream)
+  # Sends all spooled messages to +receiver+, in order. Once a message is sent,
+  # it's removed from the spool.
+  def replay(receiver)
     while message = @messages.shift
       m, args, block = message
-      stream.send(m, *args, &block)
+      receiver.send(m, *args, &block)
     end
   end
 end
