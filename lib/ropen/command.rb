de673bb95455ee969d5b3381352f3bc01ebb3cfc
Coda Hale <coda.hale@gmail.com>
2008-05-16 09:37:11 -0700

    Documentation for Ropen::Command and a wee bit of refactoring.

--- lib/ropen/command.rb
+++ lib/ropen/command.rb
@@ -3,22 +3,48 @@ require "ropen/events"
 require "ropen/pipe"
 require "ropen/spool"
 
-# TODO: document me
 # TODO: add timeouts for processes stalling for lack of stdin
 
+# The main class of Ropen, <tt>Ropen::Command</tt> encapsulates the execution of
+# and iteration with a child process.
 class Ropen::Command
-  attr_reader :executable, :arguments, :exit_status
+  # The fully-qualified filename of the executable.
+  attr_reader :executable
   
+  # An array of arguments to be passed to the executable.
+  attr_reader :arguments
+  
+  # The exit status of the process, or +nil+ if it has not run or terminated.
+  attr_reader :exit_status
+  
+  # Create a new command given an +executable+ filename and an optional array of
+  # +arguments+.
+  # 
+  #   cmd = Ropen::Command.new("cat", "-n", "/home/coda/my-recipes.txt")
+  # 
+  # <b>N.B.:</b> +executable+ <b>must</b> be the relative or full path of an
+  # executable file. If +executable+ does not exist on disk, or the current
+  # process does not have permission to execute it, a
+  # <tt>Ropen::InvalidExecutableError</tt> exception will be raised.
+  # 
+  # @param [String] executable the filename of the executable to run
+  # @param [Array] optional arguments to be passed to the executable
+  # @return [Ropen::Command] a command instance
+  # @yield passes itself to an option block for convenient configuration
+  # @raise Ropen::InvalidExecutableError raised when +executable+ is invalid
   def initialize(executable, *arguments)
-    @executable = File.expand_path(executable)
+    @executable = find_executable(executable)
     @arguments  = arguments
-    check_executable(executable)
     @stdout_events = Ropen::Events.new
     @stderr_events = Ropen::Events.new
     @stdin_spool = Ropen::Spool.new
     yield self if block_given?
   end
   
+  # Executes the command, triggering any callbacks. Blocks until the child
+  # process terminates.
+  # 
+  # @return [Process::Status] the status of the child process
   def run
     initialize_streams
     pid = fork do
@@ -32,26 +58,50 @@ class Ropen::Command
     finalize_streams
   end
   
+  # The STDIN IO for the child process. If the process is not currently running,
+  # returns a Ropen::Spool which will replay on the STDIN IO when the process
+  # is executed.
+  # 
+  # @return [IO] STDIN for the running process
   def stdin
     @stdin_io || @stdin_spool
   end
   
+  # An event builder for STDOUT output.
+  # 
+  # @return [Ropen::Events] event builder for STDOUT output
   def stdout
     @stdout_events
   end
   
+  # An event builder for STDERR output.
+  # 
+  # @return [Ropen::Events] event bulder for STDERR output.
   def stderr
     @stderr_events
   end
   
 private
   
+  # Creates a new set of pipes for the input and outputs of a process.
   def initialize_streams
     @stdin  = Ropen::Pipe.new
     @stdout = Ropen::Pipe.new
     @stderr = Ropen::Pipe.new
   end
   
+  # Redirects the input and output streams for a process to this instance's
+  # streams.
+  def redirect_streams
+    @stdin.bind_writer(STDIN)
+    @stdout.bind_reader(STDOUT)
+    @stderr.bind_reader(STDERR)
+  end
+  
+  # Closes the discarded portions of the open pipes. Synchronizes the STDIN
+  # writer. Returns STDIN, STDOUT, and STDERR IO instances.
+  # 
+  # @return [Array<IO>] STDIN, STDOUT, and STDERR
   def open_streams
     @stdin.close_reader
     @stdout.close_writer
@@ -60,12 +110,9 @@ private
     return [@stdin.writer, @stdout.reader, @stderr.reader]
   end
   
-  def redirect_streams
-    @stdin.bind_writer(STDIN)
-    @stdout.bind_reader(STDOUT)
-    @stderr.bind_reader(STDERR)
-  end
-  
+  # Connects the STDIN IO process, runs the events handler for STDOUT and
+  # STDERR, replays messages from the STDIN spool to STDIN, and blocks until
+  # the child process terminates, at which point its exit status is saved.
   def process_streams(stdin, stdout, stderr, child_pid)
     @stdin_io = stdin
     @stdout_events.run(stdout)
@@ -76,6 +123,8 @@ private
     @exit_status = $?
   end
   
+  # Closes any open streams, and removes the references to avoid broken pipe
+  # errors.
   def finalize_streams
     [@stdin, @stdout, @stderr].each { |s| s.close }
     @stdin = nil
@@ -84,14 +133,22 @@ private
     @stderr = nil
   end
   
-  def check_executable(executable_name)
-    unless File.exist?(@executable)
+  # Finds the full filename for +executable_name+.
+  # 
+  # @param executable_name [String] the full or relative filename of the executable
+  # @return [String] the fully qualified filename
+  # @raise [Ropen::InvalidExecutableError] if the file doesn't exist or isn't executable
+  def find_executable(executable_name)
+    executable = File.expand_path(executable_name)
+    unless File.exist?(executable)
       raise Ropen::InvalidExecutableError.new("#{executable_name} does not exist")
     end
     
-    unless File.executable?(@executable)
+    unless File.executable?(executable)
       raise Ropen::InvalidExecutableError.new("#{executable_name} is not executable")
     end
+    
+    return executable
   end
 
 end
