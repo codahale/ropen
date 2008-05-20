5c5b08b4e7f61253c53fa81b91b31f04ce89664f
Coda Hale <coda.hale@gmail.com>
2008-05-19 22:16:36 -0700

    Removed some todo items, old comments.

--- lib/ropen/events.rb
+++ lib/ropen/events.rb
@@ -1,10 +1,5 @@
 require "ropen"
 
-# TODO: would halting the callback chain make any sense?
-# TODO: expand this to include non-block events, and start/stop messages
-# TODO: split this into Event/EventHandler/EventBuilder
-
-# The Ropen::Events namespace. All the excitement is in
-# <tt>Ropen::Events::AbstractEvent</tt>.
+# The Ropen::Events namespace.
 module Ropen::Events
 end
