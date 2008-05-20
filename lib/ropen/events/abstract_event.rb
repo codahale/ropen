71a0a36383c5c706b8f76d56834ef6a8b8f7ca0c
Coda Hale <coda.hale@gmail.com>
2008-05-19 20:36:25 -0700

    Moved over to throw/catch event chain halting. (Thanks, Brian!)

--- lib/ropen/events/abstract_event.rb
+++ lib/ropen/events/abstract_event.rb
@@ -7,35 +7,35 @@ class Ropen::Events::AbstractEvent
   # An event called when the child process is started.
   # 
   # @param [Ropen::Command] command The command instance that has started.
-  # @return [Boolean] If +false+, the callback chain is halted.
+  # @throw [Symbol] If +:halt+, the callback chain is halted.
   def start(command)
-    return true
+    # called when the process starts
   end
   
   # An event called when the child process has finished.
   # 
   # @param [Ropen::Command] command The command instance that has finished.
-  # @return [Boolean] If +false+, the callback chain is halted.
+  # @throw If +:halt+, the callback chain is halted.
   def finish(command)
-    return true
+    # called when the process finishes
   end
   
   # An event called when the child process writes to +STDOUT+.
   # 
   # @param [Ropen::Command] command The command instance that is running.
   # @param [String] data The output of the child process on +STDOUT+.
-  # @return [Boolean] If +false+, the callback chain is halted.
+  # @throw If +:halt+, the callback chain is halted.
   def stdout(command, data)
-    return true
+    # called when the process writes to STDOUT
   end
   
   # An event called when the child process writes to +STDERR+.
   # 
   # @param [Ropen::Command] command The command instance that is running.
   # @param [String] data The output of the child process on +STDERR+.
-  # @return [Boolean] If +false+, the callback chain is halted.
+  # @throw If +:halt+, the callback chain is halted.
   def stderr(command, data)
-    return true
+    # called when the process writes to STDERR
   end
   
 end
