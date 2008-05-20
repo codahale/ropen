5baa5009ee5287a1af835632a0c58fcd8d29451f
Coda Hale <coda.hale@gmail.com>
2008-05-19 20:50:13 -0700

    Added Ropen::Events::EventsHandler.

---
+++ lib/ropen/events/events_handler.rb
@@ -0,0 +1,48 @@
+require "ropen/events"
+
+# TODO: document me
+
+class Ropen::Events::EventsHandler
+  attr_reader :events
+  
+  def initialize(command)
+    @command = command
+    @events  = []
+    @threads = []
+  end
+  
+  def register(event)
+    @events << event
+  end
+  
+  def run(stdout, stderr)
+    call_events(:start)
+    handle_output(stdout, :stdout)
+    handle_output(stderr, :stderr)
+  end
+  
+  # Blocks until the threads started by #run completes.
+  def finish
+    @threads.each { |t| t.join }
+    call_events(:finish)
+    @threads.clear
+  end
+  
+private
+  
+  def call_events(m, *args)
+    catch(:halt) do
+      @events.each { |e| e.send(m, *args) }
+    end
+  end
+  
+  def handle_output(stream, callback_method)
+    @threads << Thread.new do
+      until stream.eof?
+        data = stream.readpartial(1024) # TODO: smaller buffer?
+        call_events(callback_method, @command, data)
+      end
+    end
+  end
+  
+end
---
+++ spec/ropen/events/events_handler_spec.rb
@@ -0,0 +1,39 @@
+require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
+
+require "ropen/events/events_handler"
+
+describe Ropen::Events::EventsHandler do
+  
+  before(:each) do
+    @stdout = mock(:stdout)
+    @stderr = mock(:stderr)
+    @command = mock(:event)
+    @event = mock(:event)
+    @handler = Ropen::Events::EventsHandler.new(@command)
+  end
+  
+  it "should register events" do
+    @handler.register(@event)
+    @handler.register(@event)
+    @handler.events.should == [@event, @event]
+  end
+  
+  it "should run events on the output of a stream" do
+    @event.should_receive(:start).ordered
+    @event.should_receive(:stdout).with(@command, "blah").ordered
+    @event.should_receive(:stdout).with(@command, "blee").ordered
+    @event.should_receive(:stderr).with(@command, "ERROR").ordered
+    @event.should_receive(:finish).ordered
+    
+    @stdout.should_receive(:eof?).and_return(false, false, true)
+    @stdout.should_receive(:readpartial).with(an_instance_of(Numeric)).and_return("blah", "blee")
+    
+    @stderr.should_receive(:eof?).and_return(false, true)
+    @stderr.should_receive(:readpartial).with(an_instance_of(Numeric)).and_return("ERROR")
+    
+    @handler.register(@event)
+    @handler.run(@stdout, @stderr)
+    @handler.finish
+  end
+  
+end
